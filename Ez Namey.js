61a9235a516f3ac4f764446daa20936e96d4bf27
Oyelaja Hussein <premiumitng@gmail.com>
2024-06-04 23:17:33 +0200

    Random number 2 updated

--- Ez Namey.js
+++ Ez Namey.js
@@ -4,6 +4,7 @@
 
 const firstWords = ["Juicy", "Golly", "Fwenzy", "Winky", "Sunny", "Breezy", "Slick", "Zippy", "Dizzy", "Dandy"];
 const secondWords = ["Labs", "Studios", "Solutions", "Co", "Corp", "Industries", "Enterprises", "Tech", "Web", "Net"];
-const randomNumber = Math.floor(Math.random() * 10) + 0;
-const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+const randomNumber = Math.floor(Math.random() * 10);
+const randomNumber2 = Math.floor(Math.random() * 10)
+const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber2];
 console.log(`The startup: "${startupName}" contains ${startupName.length} characters.`);
