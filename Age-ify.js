dac310ee4cda346b2ecaa4f556384dd81d21f132
Oyelaja Hussein <premiumitng@gmail.com>
2024-06-04 23:07:39 +0200

    Updated the log using template lateral

--- Age-ify.js
+++ Age-ify.js
@@ -3,4 +3,4 @@
 const yearOfBirth = 1989;
 const yearFuture = 2045;
 const age = yearFuture - yearOfBirth;
-console.log("You will be " + age + " years old in " + yearFuture + ".")
+console.log(`You will be ${age} years old in ${yearFuture}. `)
