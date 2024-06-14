d90fdbecc578b5391e2b0daacadd5613bb9a8559
Oyelaja Hussein <premiumitng@gmail.com>
2024-06-14 13:07:51 +0200

    Candy helper implemented

---
+++ Candy helper.js
@@ -0,0 +1,33 @@
+const boughtCandyPrices =[];
+const candyPrices = {
+    "sweet": 0.5,
+    "chocolate": 0.7,
+    "toffee": 1.1,
+    "chewing-gum": 0.03
+};
+function addCandy(candyType, weight){
+    const pricePerGram = candyPrices[candyType];
+    if(pricePerGram){
+        const totalPrice = pricePerGram * weight;
+        boughtCandyPrices.push(totalPrice);
+    }else{
+        console.log("Invalid candy type");
+    }
+}
+const amountToSpend = Math.random() * 100;
+function canBuyMoreCandy(){
+    let totalPrice = 0;
+    for(let i = 0; i < boughtCandyPrices.length; i++){
+        totalPrice += boughtCandyPrices[i];
+    }
+    return totalPrice < amountToSpend;
+}
+
+if (canBuyMoreCandy()){
+    console.log("You can buy more, so please do!");
+}else{
+    console.log("Enough candy for you!");
+}
+
+addCandy("sweet", 20);     
+addCandy("chocolate", 30);
