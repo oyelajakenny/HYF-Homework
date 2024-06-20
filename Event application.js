3c723ace11abe9e944fd35bfa385cd7bad5a6a77
Oyelaja Hussein <premiumitng@gmail.com>
2024-06-20 14:55:04 +0200

    Added an if statement to check if event date is negative

--- Event application.js
+++ Event application.js
@@ -5,7 +5,7 @@ function getEventWeekday(eventDay) {
     }
    if (eventDay < 0){
         return console.log('Event day must be greater than 0');
-    }
+   }
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const today = new Date();
     const day = today.getDay();
