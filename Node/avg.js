a6a27000e13ab0ac6028646db442a2d8d7f152e1
Oyelaja Hussein <premiumitng@gmail.com>
2024-09-24 12:24:10 +0200

    made recommended changes

--- Node/avg.js
+++ Node/avg.js
@@ -1,25 +1,20 @@
-function calcAvgNum(num1, num2, num3){
-    return (num1 + num2 + num3) / 3
+function calcAvgNum(num) {
+  if (num.length === 0) return 0;
+  const sum = num.reduce((acc, num) => acc + num, 0);
+  return sum / num.length;
 }
+
 const args = process.argv.slice(2);
-if (args.length < 3){
-    console.log("Please provide 3 args")
-}else{
-    const num1 = parseFloat(args[0]);
-    const num2 = parseFloat(args[1]);
-    const num3 = parseFloat(args[2]);
 
-    if (isNaN(num1) || isNaN(num2) || isNaN(num3)){
-        console.log("All arguments must be numbers.")
-    }else{
-        const avg = calcAvgNum(num1, num2, num3);
-            console.log(
-              `The average of ${num1}, ${num2}, and ${num3} is: ${avg.toFixed(2)}`
-            );
+if (args.length < 0) {
+  console.log("Please provide at least one number");
+} else {
+  const num = args.map((arg) => parseFloat(arg));
 
-
-    }
-
-
-    
+  if (num.some(isNaN)) {
+    console.log("All arguments must be valid numbers.");
+  } else {
+    const average = calcAvgNum(num);
+    console.log(`The average of [${num.join(", ")}] is: ${average.toFixed(2)}`);
+  }
 }
